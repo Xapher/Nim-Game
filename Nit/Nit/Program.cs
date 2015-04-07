25f5e53e155e19ec941c0802bea30582dba9daff
Xapher <schreckengostb1@gmail.com>
2015-04-07 08:01:27 -0600

    Started Nim

--- Nit/Nit/Program.cs
+++ Nit/Nit/Program.cs
@@ -10,9 +10,41 @@ namespace Nit
     {
         static void Main(string[] args)
         {
-            string[] HeapA = new string[3];
-            string[] HeapB = new string[5];
-            string[] HeapC = new string[7];
+            string[] HeapA = { "|", "|", "|" };
+            string[] HeapB = { "|", "|", "|", "|", "|" };
+            string[] HeapC = { "|", "|", "|", "|", "|", "|", "|" };
+            int HeapTotal = 15;
+            Console.WriteLine("1. 1P V 2P");
+            Console.WriteLine("2. 1P V CPU");
+            Console.WriteLine("3. CPU V CPU");
+            string Input = Console.ReadLine();
+            switch (Input)
+            {
+                case ("1"):
+                    Console.WriteLine("Player 1 goes first, Please pick any number of sticks to take from a heap.");
+                    break;
+                case ("2"):
+                    Console.WriteLine("Player 1 goes first, Please pick any number of sticks to take from a heap.");
+                    break;
+                case ("3"):
+
+                    break;
+            }
+            foreach (string c in HeapA)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
+            foreach (string c in HeapB)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
+            foreach (string c in HeapC)
+            {
+                Console.Write(c);
+            }
+            Console.Read();
         }
     }
 }
